4618e738cb042a0d948aa9fffb2fa15a733e5aaa
bilal <me@example.com>
2022-05-22 15:07:51 +0500

    add login route

--- models/user.js
+++ models/user.js
@@ -1,18 +1,17 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    }
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
-const userModel = mongoose.model("User", userSchema)
+const userModel = mongoose.model("User", userSchema);
 module.exports = userModel;
