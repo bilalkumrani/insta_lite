a56853334a490930a02dbb3e0dd426f85f48b130
bilal <me@example.com>
2022-05-22 14:23:50 +0500

    folder structure

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "views"));
+app.use(express.static(path.resolve(__dirname, "assets")));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const PORT = 4000;
+
+app.get("/", (req, res) => {
+  return res.render("home");
+});
+
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log("there is error");
+  }
+  console.log("app is running on port: ", PORT);
+});
